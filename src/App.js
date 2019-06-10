14f6b3a178c331dd4eb712bc15d92df66e0f3914
BrockmeyerTyler <tjb101@yahoo.com>
2019-06-09 20:18:26 -0500

    Eject and fix routing with Switch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Calendar from './pages/Calendar';
 import Groceries from './pages/Groceries';
@@ -9,12 +9,14 @@ import ErrorBoundary from "./pages/ErrorBoundary";
 
 function App() {
   return <ErrorBoundary>
-    <Router>
-      <Route exact path='/' component={Home}/>
-      <Route path='/calendar' component={Calendar}/>
-      <Route path='/groceries' component={Groceries}/>
-      <Route path='/chores' component={Chores}/>
-      <Route component={Error404}/>
+    <Router basename='home'>
+      <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route path='/calendar' component={Calendar}/>
+        <Route path='/groceries' component={Groceries}/>
+        <Route path='/chores' component={Chores}/>
+        <Route component={Error404}/>
+      </Switch>
     </Router>
   </ErrorBoundary>
 }
